aed90e07b0f353009ac767ba0ca0f9df9a7efe28
mandrykk <mandrykk22@gmail.com>
2022-06-22 10:43:58 +0300

    rename

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const contactsOperations = require('../../models/contacts');
+const contactsOperations = require('../../service/contacts');
 
 const Joi = require('joi');
 const contactSchema = Joi.object({
